2b4e1ea75889a34725bdd74d9f1b89db19cd60db
Anandhu Kannan <136919830+devwithanandhukannan@users.noreply.github.com>
2026-04-23 14:14:22 +0530

    updated leap year

--- checkLeapyear.sol
+++ checkLeapyear.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 
 contract leapYear{
     function checkLeapYear(uint _year) public pure returns(bool){
+        require(_year >= 1000 && _year <= 9999, "Invalid year: must be 4-digit");
         if( _year % 4 == 0 && _year % 100 != 0 || _year % 400 == 0){
             return true;
         }return false;
