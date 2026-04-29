dfa5cee0bb3a8e1ae8e074761f40041242b39e20
Anandhu Kannan <136919830+devwithanandhukannan@users.noreply.github.com>
2026-04-29 10:39:15 +0530

    Create Token(usingmap).sol

---
+++ Token(usingmap).sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract Token {
+    mapping (uint => address) Data;
+    uint counter;
+
+    function issueToken() public {
+        counter++;
+        Data[counter] = msg.sender;
+    }
+
+    function findUser(uint user_id) public view returns (address) {
+        return Data[user_id];
+    }
+}
